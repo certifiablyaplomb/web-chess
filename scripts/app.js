7ca43f68691cc5bdf00eb148a5f5f784dd354f35
certifiablyaplomb <craftygaming1063@gmail.com>
2026-02-11 23:29:32 -0800

    finsihed all pawn quealities besides en pessant
    this includes promotion-- pawns are chilling now

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,20 @@
 import { renderBoardHtml, handleBoardInput } from "./board.js";
-
+import { pieceObjects } from './pieces.js'
 
 const board = document.querySelector('.js-board');
 
 //render board
 renderApp();
+document.querySelectorAll('.js-promotion-button').forEach((button)=>{
+    button.addEventListener('click', ()=>{
+        const promotionUi = document.querySelector('.js-promotion-ui');
+        const pieceId = promotionUi.dataset.pieceId;
+        const type = button.dataset.type;
+        pieceObjects[pieceId].type = type;
+        promotionUi.style.display = 'none';
+        renderApp()
+    })
+})  
 
 function renderApp(){
     board.innerHTML = renderBoardHtml();
@@ -13,8 +23,7 @@ function renderApp(){
         button.addEventListener('click', ()=>{
             if(handleBoardInput(button)){
                 renderApp();
-                console.log('rendered')
-            };
-        })
+ 
+    }})
 })
 }
--- scripts/pieces.js
+++ scripts/pieces.js
@@ -1,9 +1,13 @@
-
 class Piece{ //super
     type=null //string
     position=null //int
     id=null //int/index
     color=null //string 'w' || 'b'
+    // || ERRORS || \\
+    selfTakeError = new Error("You're taking your own piece!")
+    invalidMoveError = new Error("Invalid Move")
+
+
 
     constructor(type, position, id, color){
         this.type = type;
@@ -11,32 +15,111 @@ class Piece{ //super
         this.id = id;
         this.color = color;
     }
-    //anticipated to be only public function
-    //should return true if a move is succesful and false if not--
-    //in order to queue page to refresh
-    attemptMove(newPosition){
-        
-        console.log('this fucntion is meant to be fleshed out per piece');
-    }
-    //returns true if new position is location of a piece: else false
-    //does not make assumptions on validity of capture
-    _isTaking(newPosition, boardState){ //DUMB that private classes cant be inherited :(
-        if(boardState[newPosition]){
-            console.log('taking');
+    
+    // || PUBLIC || \\
+    attemptMove(newPosition, boardState){
+        //this should be at the top of each attemptMovement function 
+        const isTaking = this._isTaking(newPosition, boardState)
+        if (isTaking && this._isSameColor(newPosition, boardState)){
+            throw this.selfTakeError;
         }
+        ////////////////////////////////////////////////////////////
+        console.log('this fucntion is meant to be tailored out per piece');
     }
+
+    // || PRIVATE || \\ 
     _updatePosition(newPosition, boardState){
         boardState[Number(this.position)] = '';
         boardState[newPosition] = this;
         this.position = newPosition;
     }
+    //does not make assumptions on validity of capture
+    _isTaking(newPosition, boardState){ 
+        return boardState[newPosition]? true: false;
+    }
+    _isSameColor(newPosition, boardState){
+        return boardState[newPosition].color === this.color? true : false;
+    }
+    _rowChange(newPosition, absolute=true){
+        const c_row =  Math.floor(this.position / 8); 
+        const n_row = Math.floor(newPosition / 8);
+        return absolute? Math.abs(c_row - n_row): (c_row - n_row);
+    }
+    _columnChange(newPosition, absolute=true){
+        const c_col = this.position % 8;
+        const n_col = newPosition % 8;
+        return absolute? (Math.abs(c_col - n_col)): (c_col - n_col);
+    }
+    _isFirstMove(){
+        if ((8 <= this.position && this.position <=15) && this.color === 'b'){
+            return true;
+        }
+        else if ((48 <= this.position && this.position <=55) && this.color === 'w'){
+            return true;
+        }
+        return false;
+    }
+    _isEndOfBoard(newPosition){
+        if ((0 <= newPosition && newPosition <=7) && this.color === 'w'){
+            return true;
+        }
+        else if ((56 <= newPosition && newPosition <=63) && this.color === 'b'){
+            return true;
+        }
+         return false;
+    }
 };
+
+// || CHILD CLASSES || \\
+
 export class Pawn extends Piece{
-    attemptMove(newPosition, boardState){
-        //this._isTaking(newPosition, boardSate)
+    direction = null
+    
+
+    constructor(type, position, id, color){
+        super(type, position, id, color);
+        this.direction = this.color === 'b'? -1 : 1;
+    }
+
+    attemptMove(newPosition, boardState){ //returns true if move is valid, throws error if not
+        const isTaking = this._isTaking(newPosition, boardState);
+        const rowChange = this._rowChange(newPosition, false);
+        const colChange = this._columnChange(newPosition, false);
+
+        //prevent self take
+        if (isTaking && this._isSameColor(newPosition, boardState)){
+            throw this.selfTakeError;
+        }
+        //prevent horizontal movement and backwards movement
+        else if (rowChange != this.direction && rowChange != this.direction * 2){ //also prevents left/right movement
+            console.log('test1')
+            throw this.invalidMoveError;
+        }
+        //prevent invalid taking with pawn
+        else if (isTaking && ( ((rowChange) != this.direction) || (Math.abs(colChange) != 1)) ){
+            console.log('test2')
+            throw this.invalidMoveError;
+        }
+        //prevent diagonal movement
+        else if((Math.abs(colChange) > 0) && !isTaking){
+            console.log('test3')
+            throw this.invalidMoveError;
+        }
+        //only allow +2 movement on first move 
+        else if (rowChange===2 && !this._isFirstMove()){
+            console.log('test4')
+            throw this.invalidMoveError;
+            
+        }
+        if(this._isEndOfBoard(newPosition)){
+            const promotionUi = window.document.querySelector('.js-promotion-ui');
+            promotionUi.style.display='flex';
+            promotionUi.dataset.pieceId = this.id;
+        }
         this._updatePosition(newPosition, boardState);
         return true;
     }
+
 }
 class Rook extends Piece{}
 class Knight extends Piece{}
@@ -144,106 +227,109 @@ const pieceData = [
         id:15,
         color: 'b'
     },
+
+
     {
-        type: 'rook',
+        type: 'pawn',
         position: 48,
         id:16,
         color: 'w'
     },
     {
-        type: 'knight',
+        type: 'pawn',
         position: 49,
         id:17,
         color: 'w'
     },
     {
-        type: 'bishop',
+        type: 'pawn',
         position: 50,
         id:18,
         color: 'w'
     },
     {
-        type: 'king',
+        type: 'pawn',
         position: 51,
         id:19,
         color: 'w'
     },
     {
-        type: 'queen',
+        type: 'pawn',
         position: 52,
         id:20,
         color: 'w'
     },
     {
-        type: 'bishop',
+        type: 'pawn',
         position: 53,
         id:21,
         color: 'w'
     },
     {
-        type: 'knight',
+        type: 'pawn',
         position: 54,
         id:22,
         color: 'w'
     },
     {
-        type: 'rook',
+        type: 'pawn',
         position: 55,
         id:23,
         color: 'w'
     },
-
     {
-        type: 'pawn',
+        type: 'rook',
         position: 56,
         id:24,
         color: 'w'
     },
     {
-        type: 'pawn',
+        type: 'knight',
         position: 57,
         id:25,
         color: 'w'
     },
     {
-        type: 'pawn',
+        type: 'bishop',
         position: 58,
         id:26,
         color: 'w'
     },
     {
-        type: 'pawn',
+        type: 'king',
         position: 59,
         id:27,
         color: 'w'
     },
     {
-        type: 'pawn',
+        type: 'queen',
         position: 60,
         id:28,
         color: 'w'
     },
     {
-        type: 'pawn',
+        type: 'bishop',
         position: 61,
         id:29,
         color: 'w'
     },
     {
-        type: 'pawn',
+        type: 'knight',
         position: 62,
         id:30,
         color: 'w'
     },
     {
-        type: 'pawn',
+        type: 'rook',
         position: 63,
         id:31,
         color: 'w'
-    }
+    },
+
+    
 ]
 
-const pieceMap = {
+export const pieceMap = {
     'pawn': Pawn,
     'rook': Rook,
     'knight': Knight,
